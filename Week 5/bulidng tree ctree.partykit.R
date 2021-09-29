89b0cdf220834c17170d3bd32cb8f458d0611f09
josecarlos munoz <josecarlos.munoz12@outlook.com>
2021-09-28 22:10:44 -0500

    added other files to read

---
+++ Week 5/bulidng tree ctree.partykit.R
@@ -0,0 +1,16 @@
+rm(list = ls()) #remove all existing objects
+library(sets); library(partykit); library(TH.data)
+data("GlaucomaM", package = "TH.data")
+Gl<-GlaucomaM
+rm(GlaucomaM)
+a<-dim(Gl)
+set.seed(2)
+x<-sample(1:a[1],a[1]/3,F) #randomly select record numbers
+y<-as.integer(as.set(1:a[1])-as.set(x)) # take a complement
+Train.Gl<-Gl[y,];Test.Gl<-Gl[x,] # TL training set; TC testing set
+Gl.tree <- ctree(Class ~ ., data =Train.Gl) #build a tree model w TL
+Gl.tree
+plot(Gl.tree)
+pred.test.Gl <- predict(Gl.tree,newdata=Test.Gl[-11]) # classify TC
+table(Test.Gl$Class,pred.test.Gl, dnn = c("Actual class", "Predicted class"))
+acc.Gl.tree <- 100*sum(pred.test.Gl==Test.Gl$Class)/dim(Test.Gl)[1];acc.Gl.tree
