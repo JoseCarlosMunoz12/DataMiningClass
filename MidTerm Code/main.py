78656f5d521082dfd4e286ec562eadbf7a4cc5d2
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-10-06 19:31:13 -0500

    completed program to run PCA until eigen vectors and Values

--- MidTerm Code/main.py
+++ MidTerm Code/main.py
@@ -1,13 +1,11 @@
 import numpy as np
-import pandas as pd
 
 
-def print_hi(name):
-    m = np.loadtxt(name, dtype='i', delimiter=",")
-    print ("Printing the original square array:\n", m)
-    w, v = np.linalg.eig(m)
+def eigen(cov):
+    w, v = np.linalg.eig(cov)
     print ("Printing the Eigen values of the Given square array:\n", w)
     print ("Printing the Eigen Vectors of the Given square array:\n", v)
+    return v.transpose()
 
 
 def center_data(name):
@@ -15,9 +13,20 @@ def center_data(name):
     cm = np.mean(m, axis=0)
     print (cm)
     x_norm = m - np.mean(m, axis=0)
-    print(x_norm)
-
+    print("Center Data is:")
+    print (x_norm)
+    x_norm_t = x_norm.transpose()
+    cn = np.matmul(x_norm_t, x_norm)
+    x, y = cn.shape
+    cn = (1. / (x - 1.)) * cn
+    print ("Covariance is:")
+    print (cn)
+    eig_m = eigen(cn)
+    inv_eig_m = np.linalg.inv(eig_m)
+    RotData = np.matmul(m, inv_eig_m)
+    print ("rotated Dat is")
+    print (RotData)
+    
 
 if __name__ == '__main__':
-    print_hi('Covariance Matrix.txt')
     center_data('Data.txt')
