f37fa300b42778dc9fcf25143353ff2f1088e047
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-10-14 11:56:03 -0500

    Fixed the R program to work. Need to display data

--- HW 3/diabetes/diabetes_HW.R
+++ HW 3/diabetes/diabetes_HW.R
@@ -7,7 +7,7 @@ set.seed(500)
 ind <- sample(2,nrow(D_Data),replace = TRUE,prob = c(2/3,1/3))
 d_TrD <- D_Data[ind==1,] # Training Data
 d_TeD <- D_Data[ind==2,] # Test Data
-Cart_Model <- rpart(Class ~., data = D_TrD) # create a Tree for CART
-C45_Model <- J48(Class~., data= D_TrD)      # Create a Tree for C4.5
+Cart_Model <- rpart(class~., data = d_TrD) # create a Tree for CART
+C45_Model <- J48(class~., data= d_TrD)      # Create a Tree for C4.5
 summary(Cart_Model)
 summary(C45_Model)
