8340211779332b55f6c64681582981f514c28948
JoseCarlosMunoz12 <josecarlos.munoz12@outlook.com>
2021-10-14 06:45:51 -0500

    Added a CART model for training data
    added a CART function to train the Data

--- HW 3/diabetes/diabetes_HW.R
+++ HW 3/diabetes/diabetes_HW.R
@@ -1,7 +1,10 @@
 library(sets)
-df <-read.csv(file="diabetes.csv",header=TRUE)
-df$class <-as.factor(df$class)
+library(rpart)
+D_Data <-read.csv(file="diabetes.csv",header=TRUE)
+D_Data$class <-as.factor(D_Data$class)
 set.seed(500)
-ind <- sample(2,nrow(df),replace = TRUE,prob = c(2/3,1/3))
-dfTL <- df[ind==1,]
-dfTC <- df[ind==2,]
+ind <- sample(2,nrow(D_Data),replace = TRUE,prob = c(2/3,1/3))
+d_TrD <- D_Data[ind==1,] # Training Data
+d_TeD <- D_Data[ind==2,] # Test Data
+model <- rpart(Class ~., data = D_TrD)
+plot(model)
