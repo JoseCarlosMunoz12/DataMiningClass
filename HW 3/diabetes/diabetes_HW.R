1b05c8898bc734617232e0c2761cd7d15c06c223
JoseCarlosMunoz12 <josecarlos.munoz12@outlook.com>
2021-10-14 07:24:00 -0500

    Added J48
    Added j48 decision tree along with summaries of both trees

--- HW 3/diabetes/diabetes_HW.R
+++ HW 3/diabetes/diabetes_HW.R
@@ -1,10 +1,13 @@
 library(sets)
 library(rpart)
+library(RWeka)
 D_Data <-read.csv(file="diabetes.csv",header=TRUE)
 D_Data$class <-as.factor(D_Data$class)
 set.seed(500)
 ind <- sample(2,nrow(D_Data),replace = TRUE,prob = c(2/3,1/3))
 d_TrD <- D_Data[ind==1,] # Training Data
 d_TeD <- D_Data[ind==2,] # Test Data
-model <- rpart(Class ~., data = D_TrD)
-plot(model)
+Cart_Model <- rpart(Class ~., data = D_TrD) # create a Tree for CART
+C45_Model <- J48(Class~., data= D_TrD)      # Create a Tree for C4.5
+summary(Cart_Model)
+summary(C45_Model)
