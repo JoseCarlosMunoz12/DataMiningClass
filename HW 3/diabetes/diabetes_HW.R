500047a83ce9465bf760193d649c4c3182419733
josecarlos munoz <josecarlos.munoz12@outlook.com>
2021-10-16 21:32:57 -0500

    fixed plot

--- HW 3/diabetes/diabetes_HW.R
+++ HW 3/diabetes/diabetes_HW.R
@@ -2,7 +2,10 @@ library(sets)
 library(rpart)
 library(partykit)
 library(RWeka)
+
 set.seed(500)
+D_Data <- read.csv(file = "diabetes.csv", header = TRUE)
+D_Data$class <- as.factor((D_Data$class))
 ind <- sample(2,nrow(D_Data),replace = TRUE,prob = c(2/3,1/3))
 d_TrD <- D_Data[ind==1,] # Training Set
 d_TeD <- D_Data[ind==2,] # Testing Set
@@ -10,8 +13,8 @@ d_TeD <- D_Data[ind==2,] # Testing Set
 C45_Model <- J48(class~., data= d_TrD,control = Weka_control(U=TRUE,M=5))   # Create a Tree for C4.5
 CART_Model <- rpart(class~., data = d_TrD, method = "class")                #create a tree for the CART
 
-plot(C45_Model)  #Plot C45 Tree
-plot(CART_Model) #Plot CART Tree
+plot(C45_Model,unicorm=TRUE)  #Plot C45 Tree
+plot(CART_Model,uniform=TRUE) #Plot CART Tree
 
 C45_res <-predict(C45_Model,newdata=d_TeD)   #Test data with C45 Decision Tree
 CART_res <- predict(CART_Model,newdata=d_TeD, type="class") #Test data with CART Decision Tree
@@ -20,4 +23,5 @@ table(d_TeD$class,C45_res,dnn = c("actual","predicted"))  #Display Results of th
 table(d_TeD$class,CART_res,dnn = c("actual","predicted")) #Display Results of the test for CART
 
 C45_ac <- 100 * sum(C45_res==d_TeD$class) / dim(d_TeD)[1]  #C45 accuracy
-CART_ac <- 100 * sum(results==d_TeD$class) / dim(d_TeD)[1] #CART accuracy
+CART_ac <- 100 * sum(CART_res==d_TeD$class) / dim(d_TeD)[1] #CART accuracy
+
